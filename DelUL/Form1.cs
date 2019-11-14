c16ab7c036063723af8e553e6e149526db310ad0
79504 <79504@DESKTOP-2V1IQJN>
2019-11-14 20:47:58 +0300

    жирное выделение

--- DelUL/Form1.cs
+++ DelUL/Form1.cs
@@ -20,7 +20,17 @@ namespace DelUL
         private void button1_Click(object sender, EventArgs e)
         {
             StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.AppendLine("<ul>");
+            
+
+            if (richTextBox1.Lines.Count() == 1)
+                {
+                    var ClearText = richTextBox1.Lines[0];
+                    stringBuilder.Append("<b>");
+                  stringBuilder.Append(ClearText);
+                stringBuilder.Append("</b>");
+                richTextBox1.Text = stringBuilder.ToString();
+                return;
+                }
             //foreach (var line  in richTextBox1.Lines)
             //{
             //    if (!string.IsNullOrEmpty(line))
@@ -30,12 +40,14 @@ namespace DelUL
             //        stringBuilder.AppendLine("</li>");
             //    }
             //}
-           for(int i =0; i< richTextBox1.Lines.Count(); i ++)
+            stringBuilder.AppendLine("<ul>");
+            for (int i =0; i< richTextBox1.Lines.Count(); i ++)
             {
+                
                 if (!string.IsNullOrEmpty(richTextBox1.Lines[i]))
                 {
                    // richTextBox1.Lines[i] = richTextBox1.Lines[i].Trim(new char[] { '-' }).Replace("- ","");
-                    var ClearText = richTextBox1.Lines[i].Trim(new char[] { '-' }).Replace("- ", "");
+                    var ClearText = richTextBox1.Lines[i].Trim(new char[] { '-' }).Replace("- ", "").Replace("—", "") ;
                     stringBuilder.Append("<li>");
                     stringBuilder.Append(ClearText.Replace("  ", " ").Replace("\r\n", "").Replace("\t", ""));
                     if (richTextBox1.Lines.Count() - i == 1 || (richTextBox1.Lines.Count() - i == 2) && string.IsNullOrEmpty(richTextBox1.Lines.Last())) 
